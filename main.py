68685a710ff4626e56dfe2aeb9d675481dc5ddbf
Kanneli <joodhahmednaseer@gmail.com>
2024-08-13 14:09:15 +0500

    better classes

--- main.py
+++ main.py
@@ -3,16 +3,31 @@ import pygame
 
 pygame.init()
 
-pygame.display.set_caption("Platformer")
-screen = pygame.display.set_mode((640, 480))
+class Character:
+    def __init__(self) -> None:
+        self.movement = [False, False]
 
-clock = pygame.time.Clock()
+class Game:
+    def __init__(self):
+        pygame.init()
+        
+        pygame.display.set_caption("Platformer")
+        self.screen = pygame.display.set_mode((640, 480))
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+        self.clock = pygame.time.Clock()
 
-    pygame.display.update()
-    clock.tick(60)
+        self.player = Character()
+
+    def run(self):
+        while True:
+            self.screen.fill((0, 0, 0))
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            pygame.display.update()
+            self.clock.tick(60)
+
+Game().run()
